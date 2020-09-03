9ef2670078b655f0bd4c689139beea80eec4a92a
Elias Oliveira <eliasmatheus@hotmail.com>
2020-09-02 23:55:22 -0300

    Start React course

--- React/my-first-react/src/index.js
+++ React/my-first-react/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Car extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        brand: "Ford",
+        model: "Mustang",
+        color: "red",
+        year: 1964
+      };
+    }
+    render() {
+      return (
+        <div>
+          <h1>My {this.state.brand}</h1>
+          <p>
+            It is a {this.state.color}
+            {this.state.model}
+            from {this.state.year}.
+          </p>
+        </div>
+      );
+    }
+  }
+  ReactDOM.render(<Car />, document.getElementById('root'));
