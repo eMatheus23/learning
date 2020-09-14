c93b38b38e5bd546e9d395322bcbd0906dd99cee
Elias Oliveira <eliasmatheus@hotmail.com>
2020-09-13 21:47:22 -0300

    Finish Module 02 - Webpack Server

---
+++ JavaScript/starter-curso-es6/es6-app/src/main.js
@@ -0,0 +1,7 @@
+import ClasseUsuario, { idade as IdadeUsuario } from './functions'
+
+const Usuario = new ClasseUsuario();
+
+ClasseUsuario.info();
+
+console.log(IdadeUsuario);
--- JavaScript/starter-curso-es6/es6-app/webpack.config.js
+++ JavaScript/starter-curso-es6/es6-app/webpack.config.js
@@ -1,20 +1,16 @@
 module.exports = {
-    // entry é o arquivo principal
-    entry: './main.js',
-    // output é o arquivo destino, como o bundle do babel
+    entry: './src/main.js',
     output: {
-        // caminho do arquivo. __dirname se refere a mesma pasta do webpack.config.js
-        path: __dirname,
-        // nome do arquivo
+        path: __dirname + "/public",
         filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: __dirname + '/public'
     },
     module: {
         rules: [
             {
-                // "/ /" -> diz ao programa que se trata de uma expressão regular
-                // "\.js$" -> busca todos os arquivos terminados em .js
                 test: /\.js$/,
-                // para que o babel não execute os JS dentro da node_modules
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
