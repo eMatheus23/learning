640042fc76484cc99c59935cfbbf2b367b3dfa05
Elias Oliveira <eliasmatheus@hotmail.com>
2020-09-14 13:24:32 -0300

    Finish challenge 04

--- JavaScript/starter-curso-es6/es6-app/src/main.js
+++ JavaScript/starter-curso-es6/es6-app/src/main.js
@@ -1,11 +1,14 @@
 // import umPorSegundo from './challenge01';
 // import getUserFromGithub from './challenge02';
-import Github from './challenge03';
+// import Github from './challenge03';
+import buscaUsuario from './challenge04';
 
 // umPorSegundo();
 
 // getUserFromGithub("diego3g");
 // getUserFromGithub("diego3g124123");
 
-Github.getRepositories("rocketseat/rocketseat.com.br");
-Github.getRepositories("rocketseat/dslkvmskv");
+// Github.getRepositories("rocketseat/rocketseat.com.br");
+// Github.getRepositories("rocketseat/dslkvmskv");
+
+buscaUsuario("diego3g");
