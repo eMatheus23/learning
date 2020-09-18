6a9116c13a8143c935dea174896c3f1d43be97be
Elias Oliveira <eliasmatheus@hotmail.com>
2020-09-17 22:26:16 -0300

    Async api call

--- React/huntweb/src/pages/product/index.js
+++ React/huntweb/src/pages/product/index.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
+import api from '../../services/api'
 
 export default class Product extends Component {
+    state = {
+        product: {},
+    };
+
+    async componentDidMount() {
+        const { id } = this.props.params;
+
+        const response = await api.get(`/products/${id}`);
+
+        this.setState
+    }
+
+
     render() {
         return <h1>Product</h1>;
     }
