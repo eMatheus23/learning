16b150a5089eccb3a8459e2db2b6b31ca4db4be2
Elias Oliveira <eliasmatheus@hotmail.com>
2020-09-20 14:39:52 -0300

    Finish React course

--- React/huntweb/src/pages/product/index.js
+++ React/huntweb/src/pages/product/index.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import api from '../../services/api'
+import React, { Component } from "react";
+import api from "../../services/api";
+
+import "./styles.css";
 
 export default class Product extends Component {
-    state = {
-        product: {},
-    };
+  state = {
+    product: {},
+  };
 
-    async componentDidMount() {
-        const { id } = this.props.params;
+  async componentDidMount() {
+    const { id } = this.props.match.params;
 
-        const response = await api.get(`/products/${id}`);
+    const response = await api.get(`/products/${id}`);
 
-        this.setState
-    }
+    this.setState({ product: response.data });
+  }
 
+  render() {
+    const { product } = this.state;
 
-    render() {
-        return <h1>Product</h1>;
-    }
+    return (
+      <div className="product-info">
+        <h1>{product.title}</h1>
+        <p>{product.description}</p>
+
+        <p>
+          URL: <a href={product.url}>{product.url}</a>
+        </p>
+      </div>
+    );
+  }
 }
